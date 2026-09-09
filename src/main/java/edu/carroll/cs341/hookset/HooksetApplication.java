690b91083a483fc27f139220d95568623c23f8cd
JD Reighard <jreighard9226@gmail.com>
2026-09-08 21:13:05 -0600

    initial commit

---
+++ src/main/java/edu/carroll/cs341/hookset/HooksetApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs341.hookset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooksetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooksetApplication.class, args);
+	}
+
+}
