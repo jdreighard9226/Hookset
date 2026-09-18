4852b3c09b2303c2facdcc9210802b70f8734ae6
JD Reighard <jreighard9226@gmail.com>
2026-09-18 10:36:34 -0600

    added signup get implementation

--- src/main/java/edu/carroll/cs341/hookset/web/controller/SignupController.java
+++ src/main/java/edu/carroll/cs341/hookset/web/controller/SignupController.java
@@ -1,4 +1,22 @@
 package edu.carroll.cs341.hookset.web.controller;
 
+import edu.carroll.cs341.hookset.web.form.SignupForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class SignupController {
+
+    @GetMapping("/signup")
+    public String signupGet(Model model) {
+        model.addAttribute("signupForm", new SignupForm());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupPost() {
+        return "redirect:/login";
+    }
 }
