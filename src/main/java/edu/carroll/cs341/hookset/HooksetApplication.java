da76ed65ad02f12922c4fc2c92e54fd997cd3bf5
JD Reighard <jreighard9226@gmail.com>
2026-09-24 02:15:40 -0600

    added annotion for spring to go through shared java library component to find entities

--- src/main/java/edu/carroll/cs341/hookset/HooksetApplication.java
+++ src/main/java/edu/carroll/cs341/hookset/HooksetApplication.java
@@ -2,8 +2,10 @@ package edu.carroll.cs341.hookset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "shared.jpa.entity")
 public class HooksetApplication {
 
 	public static void main(String[] args) {
